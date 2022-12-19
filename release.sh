70a6bc4118ccae9b12671dfba45dfdb35712d48e
Nic Bell <nicholas.bell@indeedflex.com>
2022-12-19 19:54:17 +0000

    Snapshot test baseline.

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Releasing: $1"
+
+projectUrl="https://github.com/nicbell/emveeaye"
+mr="$projectUrl/compare/main...release/$1"
+
+# Create release branch, tag, push
+git switch develop
+git pull origin
+git checkout -b release/$1
+git tag -a "$1" -m "Release version $1"
+git push origin $1
+git push --set-upstream origin release/$1
+
+# Open URL for MR.
+open "$mr"
+
+# Generate change log
+# TODO
